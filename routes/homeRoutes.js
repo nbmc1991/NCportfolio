ffd411b3df716befe681c868e45ee8624c883768
nbmc1991 <nbmc1991@gmail.com>
2021-01-15 00:14:35 -0500

    linking handlebars to routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,6 +6,14 @@ router.get('/', (req, res) => {
     res.render('homepage');
 });
 
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
 
 
 module.exports = router; 
